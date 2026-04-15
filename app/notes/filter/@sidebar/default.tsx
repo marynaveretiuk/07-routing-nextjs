1f4547abbe0c4ee33f2ec7fe2c8ddddc068adf63
Maryna <veretiuk@gmail.com>
2026-04-14 22:00:49 -0400

    sidebar fix

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,5 +1,23 @@
-import SidebarNotes from "./SidebarNotes";
+import Link from "next/link";
+import css from "./SidebarNotes.module.css";
+import { noteTags } from "@/lib/tags";
 
 export default function DefaultSidebar() {
-  return <SidebarNotes />;
+  return (
+    <ul className={css.menuList}>
+      <li className={css.menuItem}>
+        <Link href="/notes/filter/all" className={css.menuLink}>
+          All notes
+        </Link>
+      </li>
+
+      {noteTags.map((tag) => (
+        <li key={tag} className={css.menuItem}>
+          <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
+            {tag}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
